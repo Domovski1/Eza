868ceb298a6df10852959c071a00b34fae2e729c
Domovski1 <624273242@mail.ru>
2022-04-27 14:49:49 +0300

    all

---
+++ App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Clothes.Model;
+using System.Windows;
+
+namespace Clothes
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static ClothesBaseEntities db = new ClothesBaseEntities();
+    }
+}
---
+++ Views/MaterialsPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Clothes.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для MaterialsPage.xaml
+    /// </summary>
+    public partial class MaterialsPage : Page
+    {
+        public MaterialsPage()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void TxbSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            CmbFilter.SelectedIndex = 0;
+
+
+
+            // Вывод данных из БД должен быть тут
+            MaterialList.ItemsSource = App.db.Material.ToList();
+            ;
+        }
+
+
+
+        private void CmbSort_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+
+
+        private void CmbFilter_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
